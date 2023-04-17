4ce80f73e8cfaa57931234945a147fcf3ca7f402
Nikita Gromov <nikita.gromoff@gmail.com>
2023-04-17 20:19:26 +0300

    done

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,8 +22,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(l,k) {
-  return l.value.filter(el => el != k);
+
+ function removeKFromList(l,k) {
+     
+  let li = convertListToArray(l).filter(el => el != k).reverse();
+
+ function convertListToArray (list) {
+    
+     let resArr = []
+     
+    function func(list) {
+
+     // console.log(list);
+     
+      if (list.value) resArr.push(list.value);
+      if (list.next) func(list.next);
+    }
+
+    func(list)
+    return resArr;
+}
+
+
+function convertArrayToList(arr) {
+ console.log(arr);
+    return arr.reduce((acc, cur) => {
+      if (acc) {
+        const node = new ListNode(cur);
+        node.next = acc;
+        return node;
+      }
+      
+      return new ListNode(cur);
+    }, null);
+  }
+let res = convertArrayToList(li);
+return res;
 }
 
 module.exports = {
