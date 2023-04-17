618a97ef065244b6a926b14043967d1c0965b5f2
Nikita Gromov <nikita.gromoff@gmail.com>
2023-04-17 18:02:39 +0300

    done tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,6 +6,9 @@ const { Node } = require('../extensions/list-tree.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
+
 class BinarySearchTree {
 
   constructor() {
@@ -77,6 +80,120 @@ class BinarySearchTree {
 
   remove(data) {
 
+      let parentNode = null;
+
+      let deleteFromParentNode = (curNode, parentNodeData, side) => {
+          console.log(`deleteFromParentNode >> `, curNode, parentNodeData, ` << parNodeData, side >> `, side);
+          switch (true) {
+              case curNode == null:
+                  return null;
+              case curNode.data == parentNodeData:
+                  console.log(`PAR NODE FOUND >>`, curNode);
+                  if (side == 'left') curNode.left = null;
+                  if (side == 'right') curNode.right = null;
+                  return curNode;
+              case curNode.data < data:
+                  return deleteFromParentNode(curNode.right, parentNodeData, side);
+              case curNode.data > data:
+                  return deleteFromParentNode(curNode.left, parentNodeData, side);
+          }
+      }
+
+      let oneChildNodeRemover = (curNode, parent) => {
+          let newNode;
+
+
+          if (curNode.right) {
+              newNode = curNode.right
+          }
+
+          if (curNode.left) {
+              newNode = curNode.left
+          }
+
+          if (parent == null) {
+              this.rootData = newNode;
+          }
+          else {
+              if (parent.left == curNode) {
+                  parent.left = newNode
+              }
+              else parent.right = newNode;
+          }
+          return curNode.data;
+      }
+
+      let getSmallestNode = (node) => {
+          console.log(`smallest`, node);
+          while (node.left) {
+              node = node.left;
+          }
+          return node
+      }
+
+      let twoChildNodeRemover = (curNode, parent) => {
+          let lastRight = null;
+          
+          console.log(getSmallestNode(curNode));
+      }
+
+      let nodeRemover = (curNode) => {
+
+          switch (true) {
+
+              case curNode == null:
+                  return null;
+
+              case curNode.data == data:
+                  console.log(`node to remove >> `, curNode);
+                  /* IF LAST NODE */
+                  if (!curNode.left && !curNode.right) {
+                      console.log(`last node!`);
+                      if (parentNode == null) this.rootData = null;
+                      if (parentNode.left != null && parentNode.left == curNode) {
+                          console.log(`left side node`);
+                          deleteFromParentNode(this.rootData, parentNode.data, 'left');
+                          return curNode = null;
+                      }
+                      if (parentNode.right != null && parentNode.right == curNode) {
+                          console.log(`right side node`);
+                          deleteFromParentNode(this.rootData, parentNode.data, 'right');
+                          return curNode = null;
+                      }
+
+                  }
+                  /* IF LAST NODE */
+
+
+
+                  if (!curNode.left && curNode.right || curNode.left && !curNode.right) {
+
+
+
+                      console.log(`!!!`);
+                      console.log(oneChildNodeRemover(curNode, parentNode));
+
+                  }
+
+                  if (curNode.left && curNode.right) {
+                   
+                  }
+
+
+                  return;
+
+              case curNode.data < data:
+                  parentNode = curNode;
+                  return nodeRemover(curNode.right);
+
+              case curNode.data > data:
+                  parentNode = curNode;
+                  return nodeRemover(curNode.left);
+
+          }
+      }
+      console.log(nodeRemover(this.rootData));
+
   }
 
   minmaxCollector(curNode) {
@@ -85,7 +202,7 @@ class BinarySearchTree {
       function fill(curNode) {
           if (curNode == null) return;
           arr.push(curNode.data)
-         fill(curNode.left);
+          fill(curNode.left);
           fill(curNode.right);
       }
 
